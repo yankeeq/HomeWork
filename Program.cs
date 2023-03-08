5344104f29093e2b2c92694bf343d95b0c121006
yankeeq <125729776+yankeeq@users.noreply.github.com>
2023-03-08 21:41:35 +0300

    HomrWork1

--- Program.cs
+++ Program.cs
@@ -107,7 +107,7 @@
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1,4
 
-Console.Write("Введите число: ");
+Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 for(int i = 1; i <= number; i++)
 {
